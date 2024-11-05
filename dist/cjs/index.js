33a59172c110746eb4886e603ad54bcde227fe58
andresmdg <andresmdg99@gmail.com>
2024-11-05 17:47:34 -0600

    chore(index.ts): export default

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.mgdb = void 0;
-var database_js_1 = require("./lib/database.js");
-Object.defineProperty(exports, "mgdb", { enumerable: true, get: function () { return __importDefault(database_js_1).default; } });
+exports.Database = void 0;
+const database_js_1 = __importDefault(require("./lib/database.js"));
+exports.Database = database_js_1.default;
+exports.default = database_js_1.default;
--- src/lib/database.ts
+++ src/lib/database.ts
@@ -4,11 +4,22 @@ import { existsSync, readdirSync, mkdirSync } from 'node:fs'
 import Cluster from './cluster.js'
 import { dbOptions } from './types.js'
 
+/**
+ * Representa una base de datos que contiene múltiples clústeres.
+ * Cada clúster puede contener varias colecciones.
+ */
 export default class Database {
+  /** Mapa de clústeres, donde la clave es el nombre del clúster y el valor es la instancia de `Cluster`. */
   public clusters: Map<string, Cluster>
+  /** El nombre del directorio principal donde se almacenan los clústeres. */
   public dirname: string
+  /** La ruta absoluta del directorio principal de la base de datos. */
   public path: string
 
+  /**
+   * Crea una nueva instancia de la base de datos.
+   * @param {dbOptions} [options={ dirname: 'db' }] - Opciones para la configuración de la base de datos. Por defecto, el directorio de la base de datos es `db`.
+   */
   constructor(options: dbOptions = { dirname: 'db' }) {
     this.clusters = new Map()
     this.dirname = options.dirname
@@ -25,6 +36,11 @@ export default class Database {
     }
   }
 
+  /**
+   * Conecta a un clúster existente o lo crea si no existe.
+   * @param {string} clusterName - El nombre del clúster al que se desea conectar.
+   * @returns {Cluster} La instancia del clúster conectado.
+   */
   connect(clusterName: string) {
     const existing = this.clusters.get(clusterName)
     if (existing) return existing
