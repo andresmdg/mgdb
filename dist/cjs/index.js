920c82689b7c9371b1ffedf2cad70de24f476498
andresmdg <andresmdg99@gmail.com>
2024-11-05 18:32:51 -0600

    chore(index.ts): export default changes

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
+exports.Database = void 0;
 var database_js_1 = require("./lib/database.js");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return __importDefault(database_js_1).default; } });
+Object.defineProperty(exports, "Database", { enumerable: true, get: function () { return __importDefault(database_js_1).default; } });
